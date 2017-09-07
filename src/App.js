b4b0fc87f1b1105c7e077ae86591705372f375c6
mhrice <matthewrice11@yahoo.com>
2017-09-07 00:17:47 -0700

    Version 1.0 complete!

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { HashRouter, Route, Switch } from 'react-router-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 // import './styles/styles.scss';
-import './styles/styles.scss';
+// import './styles/styles.scss';
 
 
 //components
